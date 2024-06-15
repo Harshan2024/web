c140021a781dd6a51666f2531cb99bcb17e9efc2
Harshan2024 <tuty.harshan@gmail.com>
2024-06-15 08:36:17 +0530

    new login page

--- harshan.py
+++ harshan.py
@@ -1,7 +1,14 @@
 from utils import *
 from main import *
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,session
+from flask_session import Session
 
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+                                                               
 app=Flask(__name__)
 @app.route("/<name>")
 @app.route("/",methods=["GET","POST"])
